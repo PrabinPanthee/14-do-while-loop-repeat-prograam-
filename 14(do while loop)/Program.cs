8fa9791d1da0a3e7b254b94eefd1ae36fef43750
iampr <iampr@PRABIN>
2023-01-05 23:57:22 +0545

    Add project files.

---
+++ 14(do while loop)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+class Program
+{
+     static void Main()
+
+    {
+        string UserChoice = string.Empty;
+        do
+        {
+            Console.WriteLine("Enter your number ");
+            int YourChoice = int.Parse(Console.ReadLine());
+            int Start = 0;
+            while (Start <= YourChoice)
+            {
+                Console.WriteLine(Start);
+                Start = Start + 2;
+            }
+            
+            do
+            {
+                Console.WriteLine("Do You want to contunue - Yes or No");
+                UserChoice = Console.ReadLine().ToUpper();
+                if (UserChoice != "YES" && UserChoice != "NO")
+                {
+                    Console.WriteLine("Your choice is incorrect.Select Yes or No");
+                }
+            }
+            while (UserChoice != "YES" && UserChoice != "NO");
+
+        } while(UserChoice== "YES");
+        
+    }
+}
